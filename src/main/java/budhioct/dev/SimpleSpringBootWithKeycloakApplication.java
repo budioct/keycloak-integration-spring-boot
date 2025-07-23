f59f3234f6afce80dc5e4eefb74631450607d55a
Budhi Octaviansyah <budioct@gmail.com>
2025-07-23 14:50:40 +0700

    start project

---
+++ src/main/java/budhioct/dev/SimpleSpringBootWithKeycloakApplication.java
@@ -0,0 +1,13 @@
+package budhioct.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootWithKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootWithKeycloakApplication.class, args);
+	}
+
+}
